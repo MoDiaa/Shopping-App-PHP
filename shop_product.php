7157a4be524d1e2642a49b0982799ace4293ee31
moham <mohammeddiaa10@yahoo.com>
2020-12-12 15:50:05 +0200

    MilesStone 2

---
+++ shop_product.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "android_api";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$query=mysqli_query($conn,"SELECT * FROM offers");
+
+if($query)
+{
+while($row=mysqli_fetch_array($query))
+	{
+	$flag[]=$row;
+	}
+print(json_encode($flag));
+}
+mysqli_close($conn);
+?>
